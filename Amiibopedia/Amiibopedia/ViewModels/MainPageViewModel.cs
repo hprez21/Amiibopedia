05417f7f4d9f50cf61478a6b66b4da94395bbf4f
hprez21 <hprez21@users.noreply.github.com>
2018-08-25 00:43:40 -0500

    Initial Commit
    Initial Commit

---
+++ Amiibopedia/Amiibopedia/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Amiibopedia.Helpers;
+using Amiibopedia.Models;
+using Xamarin.Forms;
+
+namespace Amiibopedia.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private ObservableCollection<Amiibo> _amiibos;
+        public ObservableCollection<Character> Characters { get; set; }
+
+        public ObservableCollection<Amiibo> Amiibos
+        {
+            get => _amiibos;
+            set
+            {
+                _amiibos = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SearchCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            SearchCommand =
+                new Command(async (param) =>
+                {
+                    IsBusy = true;
+                    var character = param as Character;
+                    if (character != null)
+                    {
+                        string url = $"http://www.amiiboapi.com/api/amiibo/?character={character.name}";
+                        var service =
+                            new HttpHelper<Amiibos>();
+                        var amiibos =
+                            await service.GetRestServiceDataAsync(url);
+                        Amiibos = new ObservableCollection<Amiibo>(amiibos.amiibo);
+                    }
+                    IsBusy = false;
+                });
+        }
+
+        public async Task LoadCharacters()
+        {
+            IsBusy = true;
+            var url = "http://www.amiiboapi.com/api/character";
+            var service =
+                new HttpHelper<Characters>();
+            var characters = await service.GetRestServiceDataAsync(url);
+            Characters = new ObservableCollection<Character>(characters.amiibo);
+            IsBusy = false;
+        }
+    }
+}
---
+++ Amiibopedia/Amiibopedia/Views/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amiibopedia.ViewModels;
+using Syncfusion.SfAutoComplete.XForms;
+using Xamarin.Forms;
+
+namespace Amiibopedia
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPageViewModel ViewModel { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            ViewModel = new MainPageViewModel();
+            await ViewModel.LoadCharacters();
+            this.BindingContext = ViewModel;            
+        }
+
+        private async void Cell_OnAppearing(object sender, EventArgs e)
+        {
+            var cell = sender as ViewCell;
+            var view = cell.View;
+
+
+            view.TranslationX = -100;
+            view.Opacity = 0;
+
+            await Task.WhenAny<bool>
+                (
+                    view.TranslateTo(0, 0, 250, Easing.SinIn),
+                    view.FadeTo(1,500,Easing.BounceIn)
+                );
+
+        }
+    }
+}
